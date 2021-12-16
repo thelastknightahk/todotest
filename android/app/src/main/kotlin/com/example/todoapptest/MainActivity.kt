68a8aa503e297dab752c25b4daa86f73cd74c833
Eleos9 <eleosmacbook@gmail.com>
2021-12-17 00:44:58 +0630

    first commit

---
+++ android/app/src/main/kotlin/com/example/todoapptest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todoapptest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:todoapptest/page/notes_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blueGrey.shade900,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+      ),
+      home: NotesPage(),
+    );
+  }
+}
